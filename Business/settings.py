4666aef10af77b5b2e51b9c60232d97a35614e17
mayurganit <mayur.gupta@ganitinc.com>
2022-06-17 15:51:28 +0530

    resolve space complesity

--- Business/settings.py
+++ Business/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from re import template
+# from re import template
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
